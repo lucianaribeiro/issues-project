2173edcb0f756b6f56bf29d02b826b689eb6a100
Luciana <albuquerqueluciana54@gmail.com>
2020-10-24 17:45:55 -0300

    Resolve githubs api rate limit

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -5,12 +5,18 @@ import CardComponent from '../components/CardComponent';
 const HomePage = () => {
 
     const [data, setData] = useState([]);
-
+    const OAUTH_TOKEN = '1642ca5a68e24040b60703025fc50e3a602399b3'
+    const info = {
+        method: 'GET',
+        headers: new Headers({
+            Authorization: `token ${OAUTH_TOKEN}`,
+        })
+    }
     useEffect(() => {
         const baseURL = 'https://api.github.com/repos/facebook/react'
         //`${BASE_URL}repos/facebook/react/issues`
 
-        fetch(`${baseURL}/issues?page=1&per_page=10`)
+        fetch(`${baseURL}/issues?page=1&per_page=10`, info)
             .then(res => res.json())
             .then((result) => {
                 console.log(result)
